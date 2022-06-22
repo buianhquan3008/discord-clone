2f87422f4bb758c76224914f530f5a4f5eae644c
QuanBA <quanba@rikkeisoft.com>
2022-06-22 17:56:31 +0700

    test for io update

--- src/test/test.ts
+++ src/test/test.ts
@@ -11,41 +11,43 @@ import { assert } from "chai";
 // const assert = require("chai").assert;
 
 describe("my awesome project", () => {
-  // let io, serverSocket, clientSocket;
+  let io, serverSocket, clientSocket;
 
-  // before((done) => {
-  //   const httpServer = createServer();
-  //   io = new Server(httpServer);
-  //   httpServer.listen(() => {
-  //     const port = httpServer.address().port;
-  //     clientSocket = new Client(`http://localhost:${port}`);
-  //     io.on("connection", (socket) => {
-  //       serverSocket = socket;
-  //     });
-  //     clientSocket.on("connect", done);
-  //   });
-  // });
+  before((done) => {
+    const httpServer = createServer();
+    io = new Server(httpServer);
+    httpServer.listen(9000, () => {
+      const port = 9000;
+      clientSocket = Client(`http://localhost:${port}`);
+      io.on("connection", (socket: any) => {
+        serverSocket = socket;
+      });
+      // console.log(clientSocket);
+      clientSocket.on("connect", done);
+    });
+   
+  });
 
-  // after(() => {
-  //   io.close();
-  //   clientSocket.close();
-  // });
+  after(() => {
+    io.close();
+    clientSocket.close();
+  });
 
-  // it("should work", (done) => {
-  //   clientSocket.on("hello", (arg) => {
-  //     assert.equal(arg, "world");
-  //     done();
-  //   });
-  //   serverSocket.emit("hello", "world");
-  // });
+  it("should work", (done) => {
+    clientSocket.on("hello", (arg) => {
+      assert.equal(arg, "world");
+      done();
+    });
+    serverSocket.emit("hello", "world");
+  });
 
-  // it("should work (with ack)", (done) => {
-  //   serverSocket.on("hi", (cb) => {
-  //     cb("hola");
-  //   });
-  //   clientSocket.emit("hi", (arg) => {
-  //     assert.equal(arg, "hola");
-  //     done();
-  //   });
-  // });
+  it("should work (with ack)", (done) => {
+    serverSocket.on("hi", (cb) => {
+      cb("hola");
+    });
+    clientSocket.emit("hi", (arg) => {
+      assert.equal(arg, "hola");
+      done();
+    });
+  });
 });
